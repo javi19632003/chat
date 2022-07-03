a5a35055207e666df9b08e2f078fec82034c19b8
javi19632003 <anicolas@transclor.com.ar>
2022-07-03 19:59:50 -0300

    entrega clase 12

--- public/main.js
+++ public/main.js
@@ -1,23 +1,31 @@
 const socket = io.connect();
 socket.on('messages', data => {
-    console.log(data);
+    console.log("on de main");
 });
 
 function render(data) {
+    console.log("render de main");
+
     const html = data.map((elem, index) => {
         return(`<div>
-            <strong>${elem.author}</strong>:
-            <em>${elem.text}</em> </div>`)
+            <strong>${elem.title}</strong>:
+            <em>${elem.price}</em>:
+            <em> ${elem.thumbail}</em> </div>`)
     }).join(" ");
     document.getElementById('messages').innerHTML = html;
 }
 
 function addMessage(e) {
+    console.log("addmessage de main");
+
     const mensaje = {
-        author: document.getElementById('username').value,
-        text: document.getElementById('texto').value
+        title: document.getElementById('title').value,
+        price: document.getElementById('price').value,
+        thumbail: document.getElementById('thumbail').value
     };
     socket.emit('new-message', mensaje);
+    console.log("emit de main");
+
     return false;
 }
 
--- server.js
+++ server.js
@@ -10,26 +10,31 @@ const PORT     = 8080;
 const httpServer            = new HttpServer(app);
 const io                    = new IOServer(httpServer);
 
-app.engine('handlebars', hand.engine());
-app.set('view engine','handlebars');
-app.set('views','./views');
+//app.engine('handlebars', hand.engine());
+//app.set('view engine','handlebars');
+//app.set('views','./views');
+
+const messages = [
+  { title: "Uno", price: 2500, thumbail:"http://un sitio web" },
+  { title: "dos", price: 1500, thumbail:"http://un sitio web" },
+  { title: "tres", price: 3500, thumbail:"http://un sitio web" }
+];
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//app.use('/', express.static(__dirname + "/public"));
+app.use('/', express.static(__dirname + "/public"));
 
-app.use('/api', router)
+//app.use('/api', router)
 
+/*
 app.get('/', (req, res) => {
     res.render('datos', { mensaje: "",
                           datoPie: ""
         });
   });
+*/
 
-app.listen(PORT, () =>{ 
-    console.log(`Servidor Http escuchando en el puerto ${PORT}`)
-})
 
 
 io.on('connection',socket => {
@@ -41,3 +46,13 @@ io.on('connection',socket => {
       io.sockets.emit('messages', messages);
   });
 });
+
+/*
+app.listen(PORT, () =>{ 
+  console.log(`Servidor Http escuchando en el puerto ${PORT}`)
+})
+*/
+httpServer.listen(8080, function() {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+})
+
